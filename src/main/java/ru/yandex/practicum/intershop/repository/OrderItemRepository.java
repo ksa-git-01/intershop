b4c3cacaa854184dd87918116a395d3f4a81f9f4
ksa <krutikov.s@gmail.com>
2025-08-20 14:58:20 +0300

    Remove files

--- src/main/java/ru/yandex/practicum/intershop/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.intershop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.intershop.model.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
