05f7b2ce9cf7a2142762cda29d93b821d429d6be
ksa <krutikov.s@gmail.com>
2025-08-21 10:12:57 +0300

    Add reactive test configuration

---
+++ src/test/java/ru/yandex/practicum/intershop/configuration/PostgresTestContainer.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.intershop.configuration;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class PostgresTestContainer extends PostgreSQLContainer<PostgresTestContainer> {
+    private static final String IMAGE = "postgres:16-alpine";
+    private static PostgresTestContainer container;
+
+    private PostgresTestContainer() { super(IMAGE); }
+
+    public static synchronized PostgresTestContainer getInstance() {
+        if (container == null) {
+            container = new PostgresTestContainer()
+                    .withDatabaseName("test")
+                    .withUsername("test")
+                    .withPassword("test");
+            container.start();
+        }
+        return container;
+    }
+}
