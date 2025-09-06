5b5ead054ea1ce86c9d9b5ddec177ca1b6f8d766
ksa <krutikov.s@gmail.com>
2025-09-06 09:51:39 +0300

    Add ServerWebInputException and DecodingException handlers

--- payment/src/main/java/ru/yandex/practicum/payment/exception/GlobalExceptionHandler.java
+++ payment/src/main/java/ru/yandex/practicum/payment/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.payment.exception;
 
+import org.springframework.core.codec.DecodingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
 import ru.yandex.practicum.payment.model.Error;
 
@@ -18,6 +20,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public Mono<ResponseEntity<Error>> handleIllegalArgument(IllegalArgumentException ex) {
         Error error = new Error("Invalid request", ex.getMessage());
+        return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error));
+    }
+
+    @ExceptionHandler(ServerWebInputException.class)
+    public Mono<ResponseEntity<Error>> handleServerWebInput(ServerWebInputException ex) {
+        Error error = new Error("Invalid request", "Некорректный формат данных");
+        return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error));
+    }
+
+    @ExceptionHandler(DecodingException.class)
+    public Mono<ResponseEntity<Error>> handleDecoding(DecodingException ex) {
+        Error error = new Error("Invalid request", "Ошибка декодирования данных");
         return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error));
     }
 
